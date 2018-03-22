2019064b65dacc1e3c2deea75a54b899b61a503f
hlx <phphlx@163.com>
2018-03-22 10:53:06 +0800

    git stash article_iface

--- src/apps/api/iface/article.iface.php
+++ src/apps/api/iface/article.iface.php
@@ -16,29 +16,29 @@ class article_iface extends base_iface {
      * @article_adate [int] 资讯发布时间(可选参数)
      * @article_stat_view [int] 浏览量(可选参数)
      */
-    public function save_action() {
+    public function save_action () {
         $this->data['article_title'] = filter::text($this->data['article_title']);
         $this->verify([
             'article_cat_id' => [
-                'code'  => '101',
-                'msg'   => '请选择资讯所属分类',
-                'rule'  => filter::$rules['integer'],
+                'code' => '101',
+                'msg'  => '请选择资讯所属分类',
+                'rule' => filter::$rules['integer'],
             ],
         ]);
         $this->data['article_cover'] = filter::normal($this->data['article_cover']);
         $this->data['article_content'] = filter::html($this->data['article_content']);
-        $this->data['article_admin_id'] = (int) $this->login['admin_id'];
-        $this->data['article_adate'] = (int) $this->data['article_adate'] ?: REQUEST_TIME;
+        $this->data['article_admin_id'] = (int)$this->login['admin_id'];
+        $this->data['article_adate'] = (int)$this->data['article_adate'] ?: REQUEST_TIME;
         $this->data['article_udate'] = REQUEST_TIME;
-        $this->data['article_stat_view'] = (int) $this->data['article_stat_view'] ?: 0;
+        $this->data['article_stat_view'] = (int)$this->data['article_stat_view'] ?: 0;
         $tab = OBJ('article_table');
         if ($tab->load($this->data)) {
             $ret = $tab->save();
             if ($ret['code'] === 0) {
                 $row_id = $this->data['article_id'] ?: $ret['row_id'];
                 $ret = OBJ('article_content_table')->replace([
-                    'article_id'        => $row_id,
-                    'article_content'   => $this->data['article_content']
+                    'article_id' => $row_id,
+                    'article_content' => $this->data['article_content'],
                 ]);
                 admin_helper::add_log($this->login['admin_id'], 'article/save', '2', ($this->data['article_id'] ? '资讯新增' : '资讯修改') . ' ID:' . $row_id);
                 $this->success('操作成功');
@@ -51,11 +51,12 @@ class article_iface extends base_iface {
      * 获取单条资讯
      * @id [int] 要获取资讯的id
      */
-    public function get_action() {
+    public function get_action () {
         $id = intval($this->data['id']);
-        $out['article'] = OBJ('article_table')->left_join('article_content_table', 'article_id', 'article_id')->get($id);
+        $out['article'] = OBJ('article_table')->left_join('article_content_table', 'article_id', 'article_id')->
+        get($id);
         $out['attrs']['category'] = category_helper::get_options(3, 0, 0);
-        foreach ((array) $out['attrs']['category'] as $k => $v) {
+        foreach ((array)$out['attrs']['category'] as $k => $v) {
             $out['attrs']['category'][$k]['space'] = str_repeat('&nbsp;&nbsp;&nbsp;', $v['cat_level']) . $v['cat_name'];
         }
         $this->success('资讯获取成功', $out);
@@ -64,7 +65,7 @@ class article_iface extends base_iface {
     /**
      * 获取资讯列表接口
      */
-    public function list_action() {
+    public function list_action () {
         $cat_ids = [];
         $arts = OBJ('article_table')->map(function ($row) use (&$cat_ids) {
             $cat_ids[$row['article_cat_id']] = 1;
@@ -73,7 +74,7 @@ class article_iface extends base_iface {
         $cat_list = OBJ('category_table')->akey('cat_id')->get_all([
             'cat_id' => array_keys($cat_ids ?: [0]),
         ]);
-        foreach ((array) $arts as $k => $v) {
+        foreach ((array)$arts as $k => $v) {
             $arts[$k]['cat_name'] = isset($cat_list[$v['article_cat_id']]) ? $cat_list[$v['article_cat_id']]['cat_name'] : '';
         }
         $this->success('资讯列表获取成功', $arts);
@@ -83,7 +84,7 @@ class article_iface extends base_iface {
      * 资讯删除接口
      * @id [int] 要删除资讯的id
      */
-    public function del_action() {
+    public function del_action () {
         $id = intval($this->data['id']);
         $ret = OBJ('article_table')->get($id);
         if (empty($ret)) {
