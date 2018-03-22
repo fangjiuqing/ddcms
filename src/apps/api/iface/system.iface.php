1cee0c292afb2a1b91d5b579283031ed5e13a4d0
hlx <phphlx@163.com>
2018-03-22 16:42:34 +0800

    '操作日志,分页优化'

--- src/apps/api/iface/system.iface.php
+++ src/apps/api/iface/system.iface.php
@@ -10,14 +10,13 @@ class system_iface extends base_iface {
      * 操作日志获取接口
      */
     public function list_action () {
-        $page = $this->data['page'] ?: 1;
-        $offset = 10;
-        $start = ($page - 1) * $offset;
+        $tab = OBJ('admin_log_table');
+        $paging = new paging_helper($tab, $this->data['pn'] ?: 1, 12);
         $user_ids = [];
-        $arts = OBJ('admin_log_table')->map(function ($row) use (&$user_ids) {
+        $arts = $tab->map(function ($row) use (&$user_ids) {
             $user_ids[$row['al_admin_id']] = 1;
             return $row;
-        })->limit($start . ',' . $offset)->get_all();
+        })->get_all();
         $user_list = OBJ('admin_table')->akey('admin_id')->get_all([
             'admin_id' => array_keys($user_ids ?: [0]),
         ]);
@@ -32,6 +31,9 @@ class system_iface extends base_iface {
             $arts[$k]['al_action_id'] = $id_title[0];
             $arts[$k]['al_title'] = rtrim($id_title[1], ']');
         }
-        $this->success('操作日志获取成功', ['list' => $arts]);
+        $this->success('操作日志获取成功', [
+            'list'   => array_values($arts),
+            'paging' => $paging->to_array(),
+        ]);
     }
 }
