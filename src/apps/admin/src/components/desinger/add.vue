b0348d9bd7bf63c7fc863b4dd2d3e1ac66f96a2a
fangwei <123763712@qq.com>
2018-04-13 09:50:03 +0800

    设计师模块

---
+++ src/apps/admin/src/components/desinger/add.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="profile">
+    <breadcrumbs :items="items">
+      <breadcrumb-item v-for="(v, i) in items" v-bind:key="i" :active="i === items.length - 1" :to="{path: v.to}" >
+        {{v.text}}
+      </breadcrumb-item>
+      <breadcrumb-item active class="pull-right">
+        <a class="btn btn-xs btn-info pull-right">
+          <i class="fa fa-plus-square"></i> 新增
+        </a>
+      </breadcrumb-item>
+    </breadcrumbs>
+    <div class="app_page" style="padding-right:15px">
+      <form action="" method="post" accept-charset="utf-8">
+        <div class="row" style="width: 91.66666667%;margin:0 auto;">
+          <div class="col-sm-7" style="padding-left:0">
+              <div class="form-group">
+                  <input class="form-control" name="des_title" v-model="form.des_title"  v-focus="form.des_title"  type="text" placeholder="资讯标题">
+              </div>
+
+              <div class="form-group">
+                  <input class="form-control" name="des_via" v-model="form.des_via"  v-focus="form.des_via"  type="text" placeholder="资讯来源">
+              </div>
+
+              <div class="form-group">
+                  <select v-model="form.des_cat_id"  name="des_cat_id" class="form-control">
+                    <option v-for="(v) in categories" v-bind:key="v.cat_id" :value="v.cat_id" v-html="v.space">
+                    </option>
+                  </select>
+              </div>
+          </div>
+          <div class="col-sm-5">
+            <img class="preview_des_cover" style="width: 200px; height: 120px;" :src="des_cover" @click="upload_cover">
+            <input type="hidden" name="des_cover" v-model="form.des_cover">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-11" style="margin:0 auto; float: none">
+            <vue-editor ref="editor" id="editor"
+              useCustomImageHandler
+              @imageAdded="upload_image" v-model="form.des_content">
+            </vue-editor>
+            <btn type="success" v-on:click="save" class="btn btn-success pull-right">保存</btn>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import { VueEditor } from 'vue2-editor'
+export default {
+  name: 'DesingerAdd',
+  components: {VueEditor},
+  metaInfo () {
+    return {
+      title: '品牌管理 - 道达智装'
+    }
+  },
+  data () {
+    return {
+      items: [
+        {text: '首页', to: '/'},
+        {text: '资讯', to: '/desinger'},
+        {text: '编辑', href: '#'}
+      ],
+      id: this.$route.query['id'] || 0,
+      form: {},
+      des_cover: '',
+      extra: {},
+      categories: []
+    }
+  },
+  methods: {
+    on_cover_error (msg) {
+      this.$loading.hide()
+      this.$notify({
+        content: msg,
+        duration: 2000,
+        type: 'danger',
+        dismissible: false
+      })
+    },
+    on_cover_start (e) {
+      this.$loading.show({
+        msg: '文件上传中, 已发送 0 % ...'
+      })
+    },
+    on_cover_finish (d) {
+      this.$loading.hide()
+      this.$notify({
+        content: '上传成功',
+        duration: 1000,
+        type: 'success',
+        dismissible: false
+      })
+      this.form.desinger_cover = d.image
+      this.desinger_cover = d.thumb
+    },
+    on_cover_progress (e) {
+      if (e) {
+        this.$loading.show({
+          msg: '文件上传中, 已发送 ' + e + ' % ...'
+        })
+      }
+    },
+    on_editor_error (msg) {
+      this.$loading.hide()
+      this.$notify({
+        content: msg,
+        duration: 2000,
+        type: 'danger',
+        dismissible: false
+      })
+    },
+    on_editor_start (e) {
+      this.$loading.show({
+        msg: '文件上传中, 已发送 0 % ...'
+      })
+    },
+    on_editor_finish (d) {
+      this.$loading.hide()
+      this.$notify({
+        content: '上传成功',
+        duration: 1000,
+        type: 'success',
+        dismissible: false
+      })
+      let url = d.big
+      this.extra.Editor.insertEmbed(this.extra.cursorLocation, 'image', url)
+      this.extra.resetUploader()
+    },
+    on_editor_progress (e) {
+      if (e) {
+        this.$loading.show({
+          msg: '文件上传中, 已发送 ' + e + ' % ...'
+        })
+      }
+    },
+    upload_image (file, Editor, cursorLocation, resetUploader) {
+      let formData = new FormData()
+      formData.append('raw', JSON.stringify({
+        'uri': 'upload/image',
+        'access_token': this.$sess.access_token
+      }))
+      formData.append('file', file)
+      this.extra = {Editor, cursorLocation, resetUploader}
+      this.$uploader.exec({
+        uri: 'upload/image',
+        el: this,
+        pre: 'editor',
+        data: formData
+      })
+    },
+    upload_cover () {
+      this.$uploader.select({
+        uri: 'upload/image',
+        el: this,
+        pre: 'cover'
+      })
+    },
+    modify: function (id) {
+      this.$loading.show({
+        msg: '加载中 ...'
+      })
+      this.$http.get('desinger', {id: this.id || 0, attrs: 1}).then(d => {
+        this.$loading.hide()
+        if (d.code === 0) {
+          this.form = this.id ? d.data.row : {}
+          this.desinger_cover = this.form['desinger_cover_thumb'] || ''
+          this.categories = d.data.category
+        } else {
+          this.form = []
+        }
+      })
+    },
+    save: function () {
+      this.$loading.show({
+        msg: '加载中 ...'
+      })
+      this.$http.save('desinger', this.form).then(d => {
+        this.$loading.hide()
+        if (d.code === 0) {
+          this.$router.push({
+            path: '/desinger'
+          })
+        } else {
+          this.$notify({
+            content: d.msg,
+            duration: 1500,
+            type: 'danger',
+            dismissible: false
+          })
+        }
+      })
+    }
+  },
+  mounted: function () {
+    this.$store.state.left_active_key = '/desinger'
+    this.modify()
+  },
+  destroyed: function () {
+    this.$loading.hide()
+  },
+  activated: function () {
+    this.$store.state.left_active_key = '/desinger'
+  }
+}
+</script>
+
+<style>
+  #editor {
+  }
+  .quillWrapper {
+    position: relative;
+  }
+  .quillWrapper .ql-snow.ql-toolbar {
+    width: 100%;
+  }
+  .ql-editor {
+    min-height: 360px;
+  }
+  .ql-toolbar {
+    text-align: left;
+  }
+</style>
---
+++ src/apps/admin/src/components/desinger/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="profile">
+    <breadcrumbs :items="items">
+      <breadcrumb-item v-for="(v, i) in items" v-bind:key="i" :active="i === items.length - 1" :to="{path: v.to}" >
+        {{v.text}}
+      </breadcrumb-item>
+      <breadcrumb-item active class="pull-right">
+        <router-link :to="{path:'/desinger/add'}" class="btn btn-xs btn-info pull-right">
+          <i class="fa fa-plus-square"></i> 新增
+        </router-link>
+      </breadcrumb-item>
+    </breadcrumbs>
+    <div class="app_page">
+      <form action="/" id="profile_form" class="form-horizontal ng-untouched ng-pristine ng-valid" method="post" novalidate="">
+        <div class="app_content">
+          <div class="content table-responsive">
+            <table class="table table-striped">
+              <thead>
+                  <tr>
+                    <th class="text-left">标题</th>
+                    <th class="text-center" width="120">分类</th>
+                    <th class="text-center" width="80">浏览</th>
+                    <th class="text-center" width="150">时间</th>
+                    <th class="text-center" width="100"></th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="(v) in rows" :key="v.des_id">
+                      <td class="text-left">
+                        <a @click="modify(v.des_id)">{{v.des_title}}</a>
+                      </td>
+                      <td class="text-center">
+                        <small>{{v.cat_name}}</small>
+                      </td>
+                      <td class="text-center">
+                        <code>{{v.des_stat_view}}</code>
+                      </td>
+                      <td class="text-center">
+                        <small>{{v.des_udate|time('yyyy-mm-dd HH:MM:ss')}}</small>
+                      </td>
+                      <td class="text-center">
+                          <btn class="btn btn-xs btn-success" @click="modify(v.des_id)"><i class="fa fa-pencil"></i></btn>
+                          <btn class="btn btn-xs btn-rose" @click="del(v.des_id)"><i class="fa fa-trash-o"></i></btn>
+                      </td>
+                  </tr>
+              </tbody>
+            </table>
+            <pagination v-model="pn" :total-page="total" @change="refresh" size="sm"/>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'desinger',
+  metaInfo () {
+    return {
+      title: '资讯管理 - 道达智装'
+    }
+  },
+  data () {
+    return {
+      items: [
+        {text: '首页', to: '/'},
+        {text: '设计师', to: '/desinger'},
+        {text: '列表', href: '#'}
+      ],
+      rows: [],
+      attrs: {},
+      pn: 1,
+      total: 1
+    }
+  },
+  methods: {
+    modify (id) {
+      this.$router.push({
+        path: '/desinger/add',
+        query: {id}
+      })
+    },
+    refresh: function () {
+      this.$loading.show({
+        msg: '加载中 ...'
+      })
+      this.$http.list('desinger', {pn: this.pn}).then(d => {
+        this.$loading.hide()
+        console.log(d)
+        if (d.code === 0) {
+          this.rows = d.data.list
+          this.pn = d.data.paging.pn
+          this.total = d.data.paging.max
+        } else {
+          this.rows = []
+        }
+      })
+    },
+    del: function (id) {
+      this.$loading.show({
+        msg: '加载中 ...'
+      })
+      this.$http.del('desinger', {id: id}).then(d => {
+        this.$loading.hide()
+        if (d.code === 0) {
+          this.$notify({
+            content: d.msg,
+            duration: 2000,
+            type: 'success',
+            dismissible: false
+          })
+          this.refresh()
+        } else {
+          this.$notify({
+            content: d.msg,
+            duration: 2000,
+            type: 'danger',
+            dismissible: false
+          })
+        }
+      })
+    }
+  },
+  mounted: function () {
+    this.$store.state.left_active_key = '/desinger'
+    this.refresh()
+  },
+  destroyed: function () {
+    this.$loading.hide()
+  },
+  activated: function () {
+    this.$store.state.left_active_key = '/desinger'
+    this.refresh()
+  }
+}
+</script>
