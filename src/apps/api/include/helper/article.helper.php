a1c5820a3f96379cea3c8b81e59885b6ee74721e
RGX <reginx@qq.com>
2018-04-16 17:27:38 +0800

    新增文章操作助手类

---
+++ src/apps/api/include/helper/article.helper.php
@@ -0,0 +1,17 @@
+<?php
+namespace re\rgx;
+
+/**
+ * 文章助手类
+ */
+class article_helper extends rgx {
+
+    /**
+     * 资讯状态
+     * @var [type]
+     */
+    public static $status = [
+        1       => '未发布',
+        2       => '已发布'
+    ];
+}
