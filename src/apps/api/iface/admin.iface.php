ad1d4187bf69f72d6ffc378e12694493a4a4ea3d
hlx <phphlx@163.com>
2018-04-18 13:36:25 +0800

    'admin/save'

--- src/apps/api/iface/admin.iface.php
+++ src/apps/api/iface/admin.iface.php
@@ -153,9 +153,7 @@ class admin_iface extends ubase_iface {
             ],
         ]);
         $this->data['admin_passwd'] = md5(md5($this->data['admin_passwd']) . $this->data['admin_salt']);
-        $this->data['admin_group_id'] = (int)$this->data['pag_id'];
-        var_dump($this->data['pag_id']);
-        dump($this->data);
+        $this->data['admin_group_id'] = (int)$this->data['admin_group_id'];
         $tab = OBJ('admin_table');
         if ($tab->load($this->data)) {
             $ret = $tab->save();
