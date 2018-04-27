017e43943982a5ce4f385825cbd125817ed63c43
hlx <phphlx@163.com>
2018-04-27 10:39:27 +0800

    访问量统计接口stat_helper::count()

--- src/apps/api/include/helper/stat.helper.php
+++ src/apps/api/include/helper/stat.helper.php
@@ -49,4 +49,36 @@ class stat_helper extends rgx {
      * 来源 wap
      */
     const VIA_WAP               = 2;
+    
+    /**
+     * @param int $type 内容类型
+     * @param int $id 内容id
+     * @param int $via 访问来源
+     * @param int $user_id 用户id
+     * @return bool
+     * author Fox
+     */
+    public static function count ($type, $id, $via, $user_id = 0) {
+        $tab = OBJ('stat_table');
+        $hash = md5($id . $type . $via . $_SERVER['HTTP_USER_AGENT'] . $_SERVER['REMOTE_ADDR'] . date('Y-m-d', time()));
+        if ($tab->get([
+            'stat_hash' => $hash,
+        ])) {
+            return true;
+        }
+        if ($tab->load([
+            'stat_type'     => (int)$type,
+            'stat_ref_id'   => (int)$id,
+            'stat_year'     => (int)date('Y', time()),
+            'stat_month'    => (int)date('m', time()),
+            'stat_day'      => (int)date('d', time()),
+            'stat_adate'    => REQUEST_TIME,
+            'stat_via'      => (int)$via,
+            'stat_user_id'  => $user_id,
+            'stat_hash'     => $hash,
+        ])) {
+            return $tab->insert()['code'] === 0;
+        }
+    }
+    
 }
