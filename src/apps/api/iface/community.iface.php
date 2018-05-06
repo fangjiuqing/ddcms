3b17ead41cf6a4c6832e04b722cc43e31f58c3e5
hlx <phphlx@163.com>
2018-05-06 16:47:04 +0800

    '户型列表修改'

--- src/apps/api/iface/community.iface.php
+++ src/apps/api/iface/community.iface.php
@@ -13,24 +13,17 @@ class community_iface extends ubase_iface {
      * 获取小区/户型列表接口
      */
     public function list_action () {
-        $tab = OBJ('unit_table');
+        $tab = OBJ('community_table');
         $paging = new paging_helper($tab, $this->data['pn'] ?: 1, 12);
-        $pco_ids = [];
-        $arts = $tab->map(function ($row) use (&$pco_ids) {
-            $pco_ids[$row['pu_co_id']] = 1;
-            return $row;
-        })->get_all();
         $region0 = [];
         $region1 = [];
         $region2 = [];
-        $pco_list = OBJ('community_table')->akey('pco_id')->map(function ($row) use (&$region0, &$region1, &$region2) {
+        $arts = OBJ('community_table')->akey('pco_id')->map(function ($row) use (&$region0, &$region1, &$region2) {
             $region0[$row['pco_region0'] . '0000']  = 1;
             $region1[$row['pco_region1'] . '00']    = 1;
             $region2[$row['pco_region2']]           = 1;
             return $row;
-        })->get_all([
-            'pco_id' => array_keys($pco_ids),
-        ]);
+        })->get_all();
         $region_tab = OBJ('region_table');
         $region0_list = $region_tab->akey('region_code')->get_all([
             'region_code' => array_keys($region0),
@@ -42,12 +35,6 @@ class community_iface extends ubase_iface {
             'region_code' => array_keys($region2),
         ]);
         foreach ((array)$arts as $k => $v) {
-            $arts[$k]['pco_id'] = isset($pco_list[$v['pu_co_id']]) ? $pco_list[$v['pu_co_id']]['pco_id'] : 0;
-            $arts[$k]['pco_region0'] = isset($pco_list[$v['pu_co_id']]) ? $pco_list[$v['pu_co_id']]['pco_region0'] : 0;
-            $arts[$k]['pco_region1'] = isset($pco_list[$v['pu_co_id']]) ? $pco_list[$v['pu_co_id']]['pco_region1'] : 0;
-            $arts[$k]['pco_region2'] = isset($pco_list[$v['pu_co_id']]) ? $pco_list[$v['pu_co_id']]['pco_region2'] : 0;
-            $arts[$k]['pco_name'] = isset($pco_list[$v['pu_co_id']]) ? $pco_list[$v['pu_co_id']]['pco_name'] : '';
-            $arts[$k]['pco_addr'] = isset($pco_list[$v['pu_co_id']]) ? $pco_list[$v['pu_co_id']]['pco_addr'] : '';
             $arts[$k]['pco_region0_label'] = isset($arts[$k]['pco_region0']) ?
                 $region0_list[$arts[$k]['pco_region0'] . '0000']['region_name'] : '';
             $arts[$k]['pco_region1_label'] = isset($arts[$k]['pco_region1']) ?
@@ -59,6 +46,31 @@ class community_iface extends ubase_iface {
             'list'   => array_values($arts),
             'paging' => $paging->to_array(),
         ]);
+    }
+    
+    
+    public function save_action () {
+        $this->data['pco_id'] = intval($this->data['id']);
+        $this->data['pco_region0'] = (int)substr($this->data['pco_region0'], 0, 2);
+        $this->data['pco_region1'] = (int)substr($this->data['pco_region1'], 0, 4);
+        $this->data['pco_region2'] = (int)substr($this->data['pco_region2'], 0, 6);
+        if (empty($this->data['pco_name'])) {
+            $this->failure('请输入正确的小区名');
+        }
+        if (empty($this->data['pco_addr'])) {
+            $this->data['pco_addr'] = '';
+        }
+        $tab = OBJ('community_copy_table');
+        if ($tab->load($this->data)) {
+            $ret = $tab->save();
+            if ($ret['code'] === 0) {
+                admin_helper::add_log($this->login['admin_id'], 'community/save', '2',
+                ($this->data['pco_id'] ? '小区编辑[' . $this->data['pco_id']
+                    : '小区新增[' . $ret['row_id']) . '@]');
+                $this->success('操作成功');
+            }
+        }
+        $this->failure($tab->get_error_desc(), 101);
     }
     
 }
