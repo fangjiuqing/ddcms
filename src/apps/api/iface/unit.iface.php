0da1621be2edb419976239ba11df7f09c87e8e84
hlx <phphlx@163.com>
2018-05-08 14:35:08 +0800

    'ok3'

--- src/apps/api/iface/unit.iface.php
+++ src/apps/api/iface/unit.iface.php
@@ -10,31 +10,10 @@ class unit_iface extends ubase_iface {
     
     public function list_action () {
         $id = intval($this->data['id']);
-        $tab = OBJ('community_copy_table');
-        $ret = $tab->get($id);
-        $tab->where([
-            'pco_region0' => $ret['pco_region0']
-        ]);
-        if ($ret['pco_region1']) {
-            $tab->where([
-                'pco_region1' => $ret['pco_region1'],
-            ]);
-        }
-        if ($ret['pco_region2']) {
-            $tab->where([
-                'pco_region2' => $ret['pco_region2'],
-            ]);
-        }
-        $pco_ids = [];
-        $tab->map(function ($row) use (&$pco_ids) {
-            $pco_ids[$row['pco_id']] = 1;
-        })->get_all([
-            'pco_name' => $ret['pco_name'],
-        ]);
         $unit_tab = OBJ('unit_copy_table');
         $paging = new paging_helper($unit_tab, $this->data['pn'] ?: 1, 4);
         $pu_list = $unit_tab->get_all([
-            'pu_co_id' => array_keys($pco_ids),
+            'pu_co_id' => $id,
         ]);
         foreach ((array)$pu_list as $k => $v) {
             $pu_list[$k]['pu_cover_thumb'] = IMAGE_URL . $v['pu_cover'] . '!500x309';
