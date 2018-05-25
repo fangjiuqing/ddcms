37eb21696458e9167895a7802d1239f799bc9dda
hlx <phphlx@163.com>
2018-05-25 11:58:52 +0800

    广告管理

---
+++ src/apps/api/include/helper/ad.helper.php
@@ -0,0 +1,15 @@
+<?php
+namespace re\rgx;
+
+class ad_helper extends rgx {
+    
+    /**
+     * @var array 广告状态
+     */
+    public static $ad_status = [
+        0   => '未发布',
+        1   => '已发布',
+        2   => '已过期',
+    ];
+    
+}
