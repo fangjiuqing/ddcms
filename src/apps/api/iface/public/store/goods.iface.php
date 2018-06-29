83d6fbdd47e0c0601f7be1a4f282bec35254a2f2
hlx <phphlx@163.com>
2018-06-29 15:13:14 +0800

    fix public/store/home/list --- add public/store/goods/list

--- src/apps/api/iface/public/store/goods.iface.php
+++ src/apps/api/iface/public/store/goods.iface.php
@@ -5,4 +5,61 @@ namespace re\rgx;
  * 商品接口类
  * @todo
  */
-class public_store_goods_iface extends ubase_iface {}
+class public_store_goods_iface extends base_iface {
+    
+    /**
+     * 商城商品分类
+     * @var int
+     */
+    public $type_id = 10;
+    
+    /**
+     * 商品列表
+     */
+    public function list_action () {
+        $cat_tab    = OBJ('category_table');
+        $goods_tab  = OBJ('goods_table');
+        $ret    = $cat_tab->map(function ($row) use($cat_tab, $goods_tab) {
+            //收集大分类下的所有分类id, 展示首页商品
+            $all_ids    = [];
+            $all_ids[]  = $row['cat_id'];
+            
+            $row['nodes']   = $cat_tab->map(function ($row) use(&$all_ids, $cat_tab, $goods_tab) {
+                //收集二级以下的分类id, 展示商品
+                $son_ids    = [];
+                $all_ids[]  = $row['cat_id'];
+                $son_ids[]  = $row['cat_id'];
+                
+                $row['node']    = $cat_tab->map(function ($row) use (&$all_ids, &$son_ids, $goods_tab) {
+                    $all_ids[]  = $row['cat_id'];
+                    $son_ids[]  = $row['cat_id'];
+                    //获取当前三级分类下的所有商品
+                    $row['goods']   = $goods_tab->order('goods_cat_id asc, goods_stat_sale desc')->get_all([
+                        'goods_cat_id' => $row['cat_id'],
+                        'goods_status' => store_helper::GOODS_STATUS_ONSALE,
+                    ]);
+                    return $row;
+                })->get_all(['cat_parent' => $row['cat_id']]);
+                
+                //获取当前二级(包括三级)分类下的所有商品
+                $row['goods']   = $goods_tab->order('goods_cat_id asc, goods_stat_sale desc')->get_all([
+                    'goods_cat_id'  => $son_ids,
+                    'goods_status'  => store_helper::GOODS_STATUS_ONSALE,
+                ]);
+                return $row;
+            })->get_all(['cat_parent' => $row['cat_id']]);
+    
+            //获取当前一级(二级, 三级)分类下的商品
+            $row['goods']   = $goods_tab->order('goods_cat_id asc, goods_stat_sale desc')->limit(9)->map(function
+            ($row) {
+                $row['goods_cover'] = IMAGEURL . $row['goods_cover'] . '!500x309';
+                return $row;
+            })->get_all([
+                'goods_cat_id'  => $all_ids,
+                'goods_status'  => store_helper::GOODS_STATUS_ONSALE,
+            ]);
+            return $row;
+        })->get_all(['cat_type'  => $this->type_id, 'cat_level' => 0]);
+        $this->success('', $ret);
+    }
+}
--- src/apps/api/iface/public/store/home.iface.php
+++ src/apps/api/iface/public/store/home.iface.php
@@ -13,11 +13,12 @@ class public_store_home_iface extends base_iface {
      * 商城首页分类列表
      */
     public function list_action () {
-        $out = array_values(category_helper::to_tree(OBJ('category_table')->order('cat_level asc, cat_sort desc')
-            ->get_all([
-                'cat_type'  => $this->type_id,
-                'cat_level' => [0, 1],
-            ])));
-        $this->success('', $out);
+        $this->success('', CACHE('public@home-index', function () {
+            return array_values(category_helper::to_tree(OBJ('category_table')->order('cat_level asc, cat_sort desc')
+                ->get_all([
+                    'cat_type'  => $this->type_id,
+                    'cat_level' => [0, 1],
+                ])));
+        }, 86400));
     }
 }
